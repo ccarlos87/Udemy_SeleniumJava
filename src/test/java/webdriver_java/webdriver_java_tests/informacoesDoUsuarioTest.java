24f80c9853d630cf2a288d5ff180fbb1d250b92d
Carlos Alfredo  Brito <calfredo@usersad.everis.int>
2019-05-27 17:39:06 -0300

    propertis configura e arquivo csv crido

--- src/test/java/webdriver_java/webdriver_java_tests/informacoesDoUsuarioTest.java
+++ src/test/java/webdriver_java/webdriver_java_tests/informacoesDoUsuarioTest.java
@@ -4,11 +4,15 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.concurrent.TimeUnit;
 
+import org.easetech.easytest.annotation.DataLoader;
+import org.easetech.easytest.annotation.Param;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,9 +24,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import suporte.generator;
 import suporte.screenshot;
 
+//apontar a classe/biblioteca de testes do easy test
+@RunWith(DataDrivenTestRunner.class) // não esquecer de colocar sempre o .class no final
+
+//filePaths = são os arquivos que armazenaram os dados para os testes
+//esse arquivo é gerado no diretório test
+@DataLoader(filePaths = "testAdicionarUmaInformacaoDoUsuario.csv")
+
 public class informacoesDoUsuarioTest {
 	private WebDriver navegador;
-	
+
 	@Rule
 	public TestName test = new TestName();
 
@@ -53,8 +64,8 @@ public class informacoesDoUsuarioTest {
 
 	}
 
-	// @Test
-	public void testAdicionarUmaInformacaoDoUsuario() {
+	@Test
+	public void testAdicionarUmaInformacaoDoUsuario(@Param(name = "tipo") String tipo,	@Param(name = "contato") String contato, @Param(name = "mensagem") String mensagemEsperada) {
 
 		// Clicar no botão através do seu xpath
 		navegador.findElement(By.xpath("//button[@data-target='addmoredata']")).click();
@@ -62,19 +73,19 @@ public class informacoesDoUsuarioTest {
 		WebElement popupAddMoreData = navegador.findElement(By.id("addmoredata"));
 		// Na combo de name "type" escolher a opção "phone"
 		WebElement campoType = popupAddMoreData.findElement(By.name("type"));
-		new Select(campoType).selectByVisibleText("Phone");
+		new Select(campoType).selectByVisibleText(tipo);
 		// No campo de name "contact" digitar um numero qualquer
-		popupAddMoreData.findElement(By.name("contact")).sendKeys("11999898998");
+		popupAddMoreData.findElement(By.name("contact")).sendKeys(contato);
 		// Clicar link de text "Save" que está na popup
 		popupAddMoreData.findElement(By.linkText("SAVE")).click();
 		// Validar mensagem de de "adicionado"
 		WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
 		String mensagem = mensagemPop.getText();
-		assertEquals("Your contact has been added!", mensagem);
-		
+		assertEquals(mensagemEsperada, mensagem);
+
 	}
 
-	@Test
+//	@Test
 	public void testRemoverUmContatoDeUsuario() {
 		// Clicar no elemento pelo xpath:
 //		navegador.findElement(By.xpath("//span[text()='11234566566']/following-sibling::a")).click();
@@ -85,14 +96,12 @@ public class informacoesDoUsuarioTest {
 		WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
 		String mensagem = mensagemPop.getText();
 		assertEquals("Rest in peace, dear phone!", mensagem);
-		
-//		//tirando screenshot de evideoncia para a linha de código acima
-//		String screenshotArquivo = "C:/Users/calfredo/Desktop/Teste_SS/" + generator.dataHoraParaArquivo() + test.getMethodName() + ".png";
-//		screenshot.tiraSS(navegador, screenshotArquivo);
 
-		
-		
-		
+//		//tirando screenshot de evideoncia para a linha de código acima
+		String screenshotArquivo = "C:/Users/calfredo/Desktop/Teste_SS/" + generator.dataHoraParaArquivo()
+				+ test.getMethodName() + ".png";
+		screenshot.tiraSS(navegador, screenshotArquivo);
+
 		// Aguardar até 10 segundos para que a janela desapareça
 		WebDriverWait aguardar = new WebDriverWait(navegador, 10);
 		aguardar.until(ExpectedConditions.stalenessOf(mensagemPop));
